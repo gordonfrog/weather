c9c76d7ef9244dd5b46fe5d861aa14d620de7469
gordonfrog <gordonfrog@gordonfrogs-MacBook-Pro.local>
2020-11-26 11:53:41 -0700

    init

---
+++ weather-station/src/main/java/station/StationController.java
@@ -0,0 +1,36 @@
+package station;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reading.Reading;
+import station.sendservice.SendService;
+
+@RestController
+public class StationController {
+	private static final Logger logger = LoggerFactory.getLogger(StationController.class);
+
+    @Autowired
+    private Station station;
+
+    @Autowired
+    private SendService sendService;
+    
+    @GetMapping("/station")
+    public List<Reading> retrieveAllSensors() {
+    	logger.info("/station");
+    	List<Reading> readings = station.readAllSensors();
+    	return readings;
+//        try {
+//            sendService.send(new StationReadings(station.getUUID(), readings));
+//        } catch (Exception e) {
+//            logger.error("Exception occurred sending readings", e);
+//        }
+    }
+
+}
